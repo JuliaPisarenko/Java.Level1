1d321c6b87915a620b7451e68ff514d60810725e
JuliaPisarenko <39385832+JuliaPisarenko@users.noreply.github.com>
2019-05-15 15:57:08 +0300

    Update Words.java

--- Words.java
+++ Words.java
@@ -2,11 +2,23 @@ package lesson3;
 
 import java.util.*;
 
-public class Words {
+public class Words {    
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>(Arrays.asList("one", "Two", "three", "four", "one",
+        
+        words(new ArrayList<>(Arrays.asList("one", "Two", "three", "four", "one",
                 "five", "two", "seven", "six", "six", "eight", "nine", "ONE", "three",
-                "nine", "tWo", "four", "eighT", "Five", "one"));
+                "nine", "tWo", "four", "eighT", "Five", "one")));
+
+        System.out.println("\nПроверка пароля:\n");
+
+        checkPassword("Ds1231232&@34");
+        checkPassword("s1231232&@34");
+        checkPassword("%S&sdfsSDF32&@34");
+        checkPassword("25555)))%%%");
+    }
+
+    public static void words(List<String> words){
+
         Iterator <String> iter = words.iterator();
         Map<String, Integer> words_counter = new HashMap<>();
 
@@ -17,22 +29,14 @@ public class Words {
             words_counter.put(word_lowerCase, count == null? 1 : count +1);
         }
         System.out.println(words_counter);
-        
-        System.out.println("\nПроверка пароля:\n");
-
-        checkPassword("Ds1231232&@34");
-        checkPassword("s1231232&@34");
-        checkPassword("%S&sdfsSDF32&@34");
-        checkPassword("25555)))%%%");
     }
-    
+
     public static boolean checkPassword(String password){
 
         if ( password!=null && password.length()>=8&& password.length()<=20
                 && password.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[()_@#$%]).{8,}")) {
             System.out.println("Пароль действительный");
             return true;
-
         } else
             System.out.println("Пароль должен содержать маленькие и заглавные буквы, цыфры и спецсимволы, длина пароля от 8ми до 20ти символов");
         return false;
